db40c5830f4aed1654ecdff45151a882ca454704
Nikolas Moya <nikolasmoya@gmail.com>
2023-08-05 01:19:26 +0200

    Add firmware folder + circuitpython 8.2.2 and lib bundle

--- src/base_app.py
+++ src/base_app.py
@@ -9,6 +9,6 @@ class BaseApp:
     def handle_button_down(self) -> None:
         raise (f"Handle button down not implemented for app {self.name}")
 
-    # When you print an object from BaseApp it will print it internal state
+    # When you print an object from BaseApp it will print its internal state
     def __repr__(self):
         return " ".join([f"{key}={self.__dict__[key]}" for key in self.__dict__])
