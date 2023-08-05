e75853435b2d2143ba10d8cbe7b0962724fd7270
Nikolas Moya <nikolasmoya@gmail.com>
2023-08-06 00:01:45 +0200

    Decouple device time update from ClockApp
    ClockApp should just use the localtime from the hardware and don't have to worry about updating the device time.
    
    The device time update has been moved to RealTimeClockModule and it is not responsibility of the RetroFrame class.

--- src/clock_app.py
+++ src/clock_app.py
@@ -1,26 +1,20 @@
 import time
 
 from displayio import OnDiskBitmap, TileGrid
-from rtc import RTC
 
 from src.base_app import BaseApp
 
 
 class ClockApp(BaseApp):
     name = "Clock"
-    def __init__(self, display, network):
+    def __init__(self, display, network, real_time_clock):
         super().__init__()
         self.display = display
         self.network = network
+        self.real_time_clock = real_time_clock
         self.separator_tile_index = 246
-        self.now: float = time.time()
         self.twelve_hour: bool = False
         self.last_time_string: str = ""
-        try:
-            self.time_from_api = self.update_time()
-        except:
-            self.time_from_api = time.localtime()
-        self.last_sync = time.mktime(self.time_from_api)
         self.create_clock_sprite()
     
     def create_clock_sprite(self):
@@ -64,45 +58,6 @@ class ClockApp(BaseApp):
         self.display.sprite_group[2][0] = 40
         self.display.sprite_group[3][0] = 40
         self.display.sprite_group[4][0] = 40
-
-    def parse_time(self, timestring):
-        # Separate into date and time
-        # YYYY-MM-DD
-        # HH:MM:SS.SS-HH:MM
-        date_time = timestring.split('T')
-        # Separate date into an array
-        year_month_day = date_time[0].split('-')
-        # Extract HH/MM/SS
-        hour_minute_second = date_time[1].split('.')[0].split(':')
-
-        return time.struct_time([int(year_month_day[0]),
-                                int(year_month_day[1]),
-                                int(year_month_day[2]),
-                                int(hour_minute_second[0]),
-                                int(hour_minute_second[1]),
-                                int(hour_minute_second[2]),
-                                -1, -1, -1])
-
-    def update_time(self):
-        time_url = 'http://worldtimeapi.org/api/ip'
-        time_data = self.network.network.fetch_data(time_url, json_path=[['datetime'], ['dst']])
-        time_struct = self.parse_time(time_data[0])
-        RTC().datetime = time_struct
-        return time_struct
-
-    def try_to_update_time(self):
-        try:
-            self.time_from_api = self.update_time()
-            self.last_sync = time.mktime(self.time_from_api)
-            self.last_time_string = self.hh_mm(time.localtime())
-        except Exception as e:
-            print("Update time threw an exception")
-            print(e)
-            # update_time() can throw an exception if time server doesn't
-            # respond. That's OK, keep running with our current time, and
-            # push sync time ahead to retry in 30 minutes (don't overwhelm
-            # the server with repeated queries).
-            self.last_sync += 1800  # 30 minutes in seconds
     
     def hh_mm(self, time_struct):
         if self.twelve_hour:
@@ -119,7 +74,7 @@ class ClockApp(BaseApp):
         return '{0:0>2}'.format(hour_string) + '{0:0>2}'.format(time_struct.tm_min)
 
     def handle_button_down(self):
-        self.try_to_update_time()
+        self.real_time_clock.sync_time_online()
 
     def draw_frame(self) -> float:
         now_struct = time.localtime()
@@ -143,10 +98,5 @@ class ClockApp(BaseApp):
                     # we use index + 1 because 0 is reserved for separator
                     # sprite has 4 sets of digits
                     self.display.sprite_group[index + 1][0] = digit_int + (index * 10)
-
-        # Sync with time server every 2 hours
-        if self.now - self.last_sync > 7200:
-            self.try_to_update_time()
-        self.now = time.time()
         return 0.1
     
---
+++ src/real_time_module.py
@@ -0,0 +1,56 @@
+import time
+
+from rtc import RTC
+
+
+class RealTimeClockModule:
+    seconds_in_minute = 60
+    minutes_in_hour = 60
+    def __init__(self, network, skip_connection):
+        self.network = network
+        self.skip_connection = skip_connection
+        self.last_sync = None
+        self.update_frequency = (self.seconds_in_minute * self.minutes_in_hour * 5) # 5 hours
+    
+    def _parse_time(self, timestring):
+        # Separate into date and time
+        # YYYY-MM-DD
+        # HH:MM:SS.SS-HH:MM
+        date_time = timestring.split('T')
+        # Separate date into an array
+        year_month_day = date_time[0].split('-')
+        # Extract HH/MM/SS
+        hour_minute_second = date_time[1].split('.')[0].split(':')
+
+        return time.struct_time([int(year_month_day[0]),
+                                int(year_month_day[1]),
+                                int(year_month_day[2]),
+                                int(hour_minute_second[0]),
+                                int(hour_minute_second[1]),
+                                int(hour_minute_second[2]),
+                                -1, -1, -1])
+
+    def sync_time_online(self):
+        print("Syncing time online")
+        if self.skip_connection:
+            print("Skipping online time sync because of skip_connection")
+            return
+        try:
+            time_data = self.network.get_json('http://worldtimeapi.org/api/ip')
+            time_struct = self._parse_time(time_data.get("datetime"))
+            RTC().datetime = time_struct
+            # print("Localtime after updating device", time.localtime())
+            self.last_sync = time.time()
+        except Exception as e:
+            print("Couldn't fetch time from server.")
+            print(e)
+            # Try again in 1/4 of the update frequency time
+            self.last_sync = time.time() - (self.update_frequency * 0.75) 
+
+    def check_for_time_sync(self):
+        if self.skip_connection:
+            return
+        # Sync with time server if we haven't synced yet or every `update_frequency` seconds
+        if self.last_sync is None or (time.time() - self.last_sync) > self.update_frequency:
+            self.sync_time_online()
+
