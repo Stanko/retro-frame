9b57e7a5d815e03a77026d103044c86c9db5dd06
Nikolas Moya <nikolasmoya@gmail.com>
2023-08-06 00:00:42 +0200

    Re-do network connection to save memory
    By not using the Network class from adafruit, we are saving 50kb of SRAM memory. This means we can use both network connectivity and the GifPlayerApp without running into OutOfMemory errors.

--- src/network_module.py
+++ src/network_module.py
@@ -1,4 +1,10 @@
-from adafruit_matrixportal.network import Network
+import gc
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from digitalio import DigitalInOut
+import board
+import busio
+from adafruit_esp32spi import adafruit_esp32spi
 
 try:
     from secrets import secrets
@@ -9,13 +15,46 @@ except ImportError:
 class NetworkModule:
     def __init__(self, skip_connection: bool = False):
         self.skip_connection = skip_connection
-        self.network = Network(status_neopixel=None, debug=False)
+        # Reference: https://learn.adafruit.com/adafruit-matrixportal-m4/internet-connect
+        self.esp = None
+        self.esp32_cs = DigitalInOut(board.ESP_CS)
+        self.esp32_ready = DigitalInOut(board.ESP_BUSY)
+        self.esp32_reset = DigitalInOut(board.ESP_RESET)
+        self.spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 
     def is_connected(self) -> bool:
-        return self.network.is_connected
+        if self.esp is None:
+            return False
+        return self.esp.is_connected
 
     def connect(self) -> None:
         if not self.skip_connection:
-            self.network.connect()
-        else:
-            print("Network connection skipped")
+            retries = 3
+            print("Connecting to WiFi...")
+            self.esp = adafruit_esp32spi.ESP_SPIcontrol(self.spi, self.esp32_cs, self.esp32_ready, self.esp32_reset)
+            while not self.esp.is_connected and retries > 0:
+                try:
+                    self.esp.connect_AP(secrets["ssid"], secrets["password"])
+                except Exception as e:
+                    retries -= 1
+                    print(e)
+                    print(f"Could not connect to access point {secrets['ssid']}. Retries {retries}...")
+                    continue
+            print("Connected to", str(self.esp.ssid, "utf-8"), "\tRSSI:", self.esp.rssi)
+            socket.set_interface(self.esp)
+            requests.set_socket(socket, self.esp)
+            gc.collect()
+    
+    def disconnect(self) -> None:
+        if not self.skip_connection:
+            print("Disconnecting from WiFi...")
+            self.esp.disconnect()
+            self.esp = None
+            gc.collect()
+    
+    def get_json(self, url: str):
+        if not self.is_connected():
+            self.connect()
+        response = requests.get(url)
+        gc.collect()
+        return response.json()
