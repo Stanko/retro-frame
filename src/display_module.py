0bc2fb1297715b226eb64afeeb5ac82247c33e70
Nikolas Moya <1767648+nmoya@users.noreply.github.com>
2026-03-23 02:14:25 +0100

    Update display_module for backwards compatibility

--- src/display_module.py
+++ src/display_module.py
@@ -8,7 +8,10 @@ class DisplayModule:
     def __init__(self, width: int, height: int, bit_depth: int):
         self.matrix = Matrix(width=width, height=height, bit_depth=bit_depth)
         self.sprite_group = Group()
-        self.matrix.display.show(self.sprite_group)
+        if hasattr(self.matrix.display, "root_group"):
+            self.matrix.display.root_group = self.sprite_group
+        else:
+            self.matrix.display.show(self.sprite_group)
 
     def clear(self) -> None:
         while self.sprite_group:
