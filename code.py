27030c374c72dc565d511df57406a07567e454ea
Nikolas Moya <1767648+nmoya@users.noreply.github.com>
2026-03-24 23:03:30 +0100

    Add i2c diagnostic hint in code.py

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 # import src.diagnostics.matrix_diagnostic
 # import src.diagnostics.bit_depth_diagnostic
 # import src.diagnostics.font_diagnostic
+# import src.diagnostics.i2c_seesaw_diagnostic
 import src.retro_frame
