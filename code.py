daf618cfce1972db7d2db7c042ebb1b066894667
Nikolas Moya <1767648+nmoya@users.noreply.github.com>
2026-03-30 21:17:11 +0200

    Add UI components diagnostic code

--- code.py
+++ code.py
@@ -2,4 +2,5 @@
 # import src.diagnostics.bit_depth_diagnostic
 # import src.diagnostics.font_diagnostic
 # import src.diagnostics.i2c_seesaw_diagnostic
+# import src.diagnostics.ui_components_diagnostic
 import src.retro_frame
